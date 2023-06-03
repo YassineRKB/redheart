993ea8279dc97e6b55f5bc62c580c1c23574d3cd
Yassine Rakibi <yassine@arcraven.com>
2023-06-03 07:22:40 +0200

    proper headers import & project structure

--- libs.h
+++ libs.h
@@ -1,3 +1,15 @@
 #ifndef _LIBS_H
+// headers import
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <openssl/evp.h>
+
+// magic numbers
+
+// prototypes
+
 #define _LIBS_H
 #endif
